2732c246ab2a84161d1fc8530b095c5d915806f3
tobino49 <guennec.leon@gmail.com>
2023-10-24 01:18:39 +0200

    initialisation des fichiers

---
+++ background.js
@@ -0,0 +1,8 @@
+// https://developer.chrome.com/docs/extensions/mv3/manifest/
+console.log('yes')
+
+// const currentVideo = document.getElementById('container')
+
+// if (currentVideo.querySelector('div.video-ads')){
+//   console.log('ADS DETECTED');
+// }
